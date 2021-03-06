c5932186e904d0ddd7db43572e2db57d9875f022
Pratish Jage <pratish549@gmail.com>
2021-03-06 18:42:43 +0530

    add test for data file

---
+++ app/src/main/java/com/app/nasa/home/model/NasaDataModel.kt
@@ -0,0 +1,22 @@
+package com.app.nasa.home.model
+
+import com.beust.klaxon.Json
+
+data class NasaDataModel(
+  @Json(name = "copyright")
+  val copyright: String? = null,
+  @Json(name = "date")
+  val date: String?,
+  @Json(name = "explanation")
+  val explanation: String?,
+  @Json(name = "hdurl")
+  val hdUrl: String?,
+  @Json(name = "media_type")
+  val media_type: String?,
+  @Json(name = "service_version")
+  val service_version: String?,
+  @Json(name = "title")
+  val title: String?,
+  @Json(name = "url")
+  val url: String?
+)
